703d0638f0552ead5c19bf5fae5a64dac9f79f99
Lara Bertram <lara.bertram@study.hs-duesseldorf.de>
2022-03-27 21:11:42 +0200

    Begin implementing task definition

--- UsabilEval/src/dyneval/components/TaskDefinition.vue
+++ UsabilEval/src/dyneval/components/TaskDefinition.vue
@@ -2,17 +2,15 @@
     <div>
         <div class="input-with-button">
             <input id="taskname" class="input" type="text" placeholder="Aufgabe">
-            <button class="button button--primary" @click="handleClick">Erstellen</button>
+            <button class="button button--primary" @click="addTask">Erstellen</button>
         </div>
-        <p v-if="tasks.length === 0" class="type--pos-medium-normal">
+        <p v-if="!hasTasks" class="type--pos-medium-normal">
             Wähle einen Namen für die Aufgabe und erstelle sie.
         </p>
-        <!-- Wenn keine Aufgaben da Aufforung, sonst Liste mit Aufgaben -->
-        <!-- -->
-        <!-- <hr class="style-one">
-        <p class="type--pos-medium-bold">
-            Deine Aufgaben
-        </p> -->
+        <div v-else-if="hasTasks" class="type--pos-medium-normal">
+            <p>Füge die einzelnen Bearbeitungsschritte der Aufgabe hinzu.</p>
+            <button class="button button--primary">Hinzufügen</button>
+        </div>
         <div id="tasks" class="type--pos-medium-normal">
         </div>
     </div>
@@ -24,20 +22,67 @@ export default {
     name: 'TaskDefinition',
     data() {
         return {
-            tasks: [],
+            hasTasks: false,
         }
     },
     methods: {
-        handleClick() {
+        addTask() {
 			let taskname = document.getElementById('taskname').value;
-            this.tasks.push(taskname);
 			// wenn taskname leer Fehler zurückgeben
-			let element = document.getElementById('tasks');
-			let child = document.createElement('div');
-			child.innerHTML = '<p>' + taskname + '</p>';
-			element.appendChild(child);
+
+			let tasks = document.getElementById('tasks');
+
+            // create element for task annotation
+            var element = document.createElement('div');
+            element.id = 'annotation-' + taskname;
+
+            // create title element for color square and task title
+			let taskTitle = document.createElement('div');
+            taskTitle.id = 'annotation-title-' + taskname;
+			taskTitle.classList.add('annotation-list-entry');
+			taskTitle.classList.add('type--pos-medium-normal');
+
+            // create color square for title
+            var colorSquare = document.createElement('div');
+            colorSquare.classList.add('annotation-list-entry-colorsquare');
+            colorSquare.style.backgroundColor = this.getRandomColor();
+            // check if color already used
+
+            // create text content for title
+            var textContent = document.createTextNode(taskname);
+
+            // create entry symbols for edit and delete
+			var trashSymbol = document.createElement('div');
+			trashSymbol.id = 'delete-' + taskname;
+			trashSymbol.classList.add('icon');
+			trashSymbol.classList.add('icon--trash');
+			trashSymbol.onclick = function() {
+				var annotation = document.getElementById('annotation-' + taskname);
+                annotation.parentNode.removeChild(annotation);
+                if (!tasks.hasChildNodes()) {
+                    this.hasTasks = false;
+                }
+			};
+
+            // add elements as child to each other
+            taskTitle.appendChild(colorSquare);
+            taskTitle.appendChild(textContent);
+            taskTitle.appendChild(trashSymbol);
+            element.appendChild(taskTitle);
+
+            // add element to task annotations
+            tasks.appendChild(element);
+
+            this.hasTasks = true;
+
             // save in task list file
 		},
+        getRandomColor() {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            return 'rgb(' + r + ', ' + g + ', ' + b +')';
+        },
     },
 }
 </script>
@@ -45,9 +90,26 @@ export default {
 <style lang='scss'>
 	@import "../../figma-ui/figma-plugin-ds";
 
+    .annotation-list-entry {
+		display: flex;
+        margin-top: 20px;
+		margin-bottom: 20px;
+	}
+
     .input-with-button {
 		display: flex;
 		justify-content: space-between;
+	}
+
+    .annotation-list-entry-colorsquare {
+		width: 15px;
+		height: 15px;
+		margin-right: 15px;
+		border-radius: 10px;
+	}
+
+    .annotation-list-entry-element-symbols {
+		display: flex;
 	}
 
 	hr.style-one {
