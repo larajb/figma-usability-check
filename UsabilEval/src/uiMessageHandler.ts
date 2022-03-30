9b4d948f3dc56f70350313e265b11c7df4d1f510
Lara Bertram <lara.bertram@study.hs-duesseldorf.de>
2022-03-30 19:01:49 +0200

    Fix adding messages with same name

--- UsabilEval/src/uiMessageHandler.ts
+++ UsabilEval/src/uiMessageHandler.ts
@@ -3,7 +3,16 @@ export const dispatch = (action: String, data?: any) => {
 	parent.postMessage({ pluginMessage: { action, data } }, '*');
 };
 export const handleEvent = (type: String, callback: Function) => {
-	eventListeners.push({ type, callback });
+	var isAdded = false;
+	for(let i = 0; i < eventListeners.length; i++) {
+		if (eventListeners[i].type === type) {
+			isAdded = true;
+			eventListeners[i].callback = callback;
+		}
+	}
+	if (!isAdded) {
+		eventListeners.push({ type, callback });
+	}
 };
 window.onmessage = event => {
 	const message = event.data.pluginMessage;
