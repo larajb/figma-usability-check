7d9a64af2da6484ed732bd7a34434ec0602d6a4a
Lara Bertram <lara.bertram@study.hs-duesseldorf.de>
2022-04-12 11:04:28 +0200

    Update goms

--- UsabilEval/src/dyneval/goms/Goms.ts
+++ UsabilEval/src/dyneval/goms/Goms.ts
@@ -2,25 +2,38 @@ import { getWidth, getCenterOfNode, getFrame } from '../../figmaAccess/nodePrope
 
 export class Goms {
 
-    homingTime: any = 0.4;
-    keystrokeTime: any = 0.2;
-    mentallyPreparingTime: any = 1.35;
-    pointingTime: any;
-    responseTime: any;
+    homingTime: number;
+    keystrokeTime: number;
+    mentallyPreparingTime: number;
+    pointingTime: number;
+    responseTime: number;
 
-    constructor (responseTime) {
+    pointingTimes: any;
+    avgPointingTime: number;
+    homingNums: any;
+    avgHomingNum: number;
+
+    constructor (responseTime: number) {
+        this.homingTime = 0.4;
+        this.keystrokeTime = 0.2,
+        this.mentallyPreparingTime = 1.35;
         this.responseTime = responseTime;
+
+        this.pointingTimes = [];
+        this.avgPointingTime = 0;
+        this.homingNums = [];
+        this.avgHomingNum = 0;
     }
 
     /**
      * This is a function that contains all steps of the goms model to calculate the time for executing a task.
      * @returns time
      */
-    public useGomsModel (steps) {
-        var convertedSteps = this.replacePatterns(steps);
-        convertedSteps = this.placeResponseTimeOperator(steps, convertedSteps);
+    public useGomsModel(task: any): number {
+        var convertedSteps = this.replacePatterns(task.steps);
+        convertedSteps = this.placeResponseTimeOperator(task.steps, convertedSteps);
         convertedSteps = this.placeMentallyPreparingOperator(convertedSteps);
-        var time = this.calculateTime(steps, convertedSteps);
+        var time = this.calculateTime(task.steps, convertedSteps);
         return time;
     }
 
@@ -29,13 +42,14 @@ export class Goms {
      * @param steps 
      * @returns resultArray
      */
-    private replacePatterns (steps) {
+    private replacePatterns(steps: Array<any>): any {
         var resultArray = [];
         var homingState = '';
-        steps.forEach(step => {
-            switch(step.type) {
+        for (let i = 0; i < steps.length; i++) {
+            switch(steps[i].type) {
                 case 'button':
-                    if (homingState === '' || homingState === 'K') {
+                    if (homingState === '' || homingState === 'k') {
+                        homingState = 'm';
                         resultArray.push('HPK');
                     } else if (homingState === 'm') {
                         resultArray.push('PK');
@@ -44,25 +58,28 @@ export class Goms {
                 case 'input':
                     var string = '';
                     if (homingState === '' || homingState === 'k') {
+                        homingState = 'm';
                         string += 'HPK';
                     } else if (homingState === 'm') {
                         string += 'PK';
                     }
+                    homingState = 'k';
                     string += 'H';
-                    for (let i = 0; i < step.input.length; i++) {
+                    for (let j = 0; j < steps[i].input.length; j++) {
                         string += 'K';
                     }
                     resultArray.push(string);
                     break;
                 case 'link':
                     if (homingState === '' || homingState === 'k') {
+                        homingState = 'm';
                         resultArray.push('HPK');
                     } else if (homingState === 'm')  {
                         resultArray.push('PK');
                     }
                     break;
-            }
-        })
+           }
+        }
         return resultArray;
     }
 
@@ -72,7 +89,7 @@ export class Goms {
      * @param convertedSteps 
      * @returns convertedSteps
      */
-    private placeResponseTimeOperator (steps, convertedSteps) {
+    private placeResponseTimeOperator(steps: any, convertedSteps: any): any {
         for (let i = 0; i < steps.length-1; i++) {
             var current = getFrame(steps[i].id);
             var next = getFrame(steps[i+1].id);
@@ -88,7 +105,7 @@ export class Goms {
      * @param convertedSteps 
      * @returns convertedSteps
      */
-    private placeMentallyPreparingOperator (convertedSteps) {
+    private placeMentallyPreparingOperator(convertedSteps: any): any {
         for (let i = 0; i < convertedSteps.length; i++) {
             var resultString = convertedSteps[i];
             for (let j = 0; j < convertedSteps[i].length; j++) {
@@ -138,13 +155,15 @@ export class Goms {
      * @param convertedSteps 
      * @returns time
      */
-    private calculateTime (steps, convertedSteps) {
+    private calculateTime(steps: any, convertedSteps: any): number {
         var time = 0.0;
         for (let i = 0; i < convertedSteps.length; i++) {
             for (let j = 0; j < convertedSteps[i].length; j++) {
+                var homingNum = 0;
                 switch(convertedSteps[i][j]) {
                     case 'H':
                         time += this.homingTime;
+                        homingNum++;
                         break;
                     case 'K':
                         time += this.keystrokeTime;
@@ -153,18 +172,32 @@ export class Goms {
                         time += this.mentallyPreparingTime;
                         break;
                     case 'P':
+                        var pointingTime = 0;
                         if (i === 0) {
-                            time += this.calculateFittsLaw(null, steps[i].id);
+                            pointingTime = this.calculateFittsLaw(null, steps[i].id);
                         } else {
-                            time += this.calculateFittsLaw(steps[i-1].id, steps[i].id);
+                            pointingTime = this.calculateFittsLaw(steps[i-1].id, steps[i].id);
                         }
+                        this.pointingTimes.push(pointingTime);
+                        time += pointingTime;
                         break;
                     case 'R':
-                        // TODO: set realistic value
+                        time += this.responseTime;
                         break;
                 }
+                this.homingNums.push(homingNum);
             }
         }
+        var pointingTimeSum = 0;
+        this.pointingTimes.forEach(time => {
+            pointingTimeSum += time;
+        });
+        this.avgPointingTime = pointingTimeSum / this.pointingTimes.length;
+        var homingNumSum = 0;
+        this.homingNums.forEach(num => {
+            homingNumSum += num;
+        })
+        this.avgHomingNum = homingNumSum / steps.length;
         return time;
     }
 
@@ -174,7 +207,7 @@ export class Goms {
      * @param currentStepId 
      * @returns time
      */
-    private calculateFittsLaw (lastStepId, currentStepId) {
+    private calculateFittsLaw(lastStepId: number, currentStepId: number): number {
         // two nodes > calculate fitts law between
         // a + b*log2(d/s + 1)
         var a = 0.05;
@@ -189,7 +222,7 @@ export class Goms {
             lastCenter = { x: 0, y: 0 };
         }
         // get center of second
-        var currentCenter = getCenterOfNode(lastStepId);
+        var currentCenter = getCenterOfNode(currentStepId);
 
         // calculate x and y for pythagoras
         var x = 0;
